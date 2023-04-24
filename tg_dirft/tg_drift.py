fc17b44a24d08faf01b6ac0a5b27f517cc1f65a4
LostGladiator <tomiweiss@live.at>
2023-04-24 21:08:02 +0200

    [core] add json logging. log level not included yet

--- tg_dirft/tg_drift.py
+++ tg_dirft/tg_drift.py
@@ -1,53 +1,70 @@
 import os
 import subprocess
 import re
+import logging
+import json_log_formatter
+
+# Create a logger
+logger = logging.getLogger()
+
+# Set a default log level for the logger
+logging.basicConfig(level=logging.INFO)
+
+# Create a JSON formatter
+formatter = json_log_formatter.JSONFormatter()
+
+# Create a stream handler and set its formatter
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(handler)
 
 
 
-
-# cmd = "terraform -chdir=/home/thomas/dev/tg_drift/test-terraform plan"
-# os.system(cmd)
 
 os.chdir("/home/thomas/dev/tg_drift/test-terragrunt/")
 os.environ["TG_CONFIG_FILE_PATH"] = "/home/thomas/dev/tg_drift/test-terragrunt/values.yaml"
-run_all_plan = subprocess.check_output(["terragrunt", "run-all", "plan", "-no-color"])
-print(run_all_plan)
 
 
 
-output_str = run_all_plan.decode('utf-8')
+def run_all_plan():
+    logging.info("Running terragrunt run-all plan...")
+    run_all_plan = subprocess.check_output(["terragrunt", "run-all", "plan", "-no-color"])
 
-matches = re.findall(r'Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', output_str)
+    output_str = run_all_plan.decode('utf-8')
+    matches = re.findall(r'Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', output_str)
 
-# Extract the numbers from the match and store them in a list of dictionaries
-plans = []
-for match in matches:
-    plan = {
-        'add': int(match[0]),
-        'change': int(match[1]),
-        'destroy': int(match[2]),
-        'resources': {}
-    }
-    plans.append(plan)
+    # Extract the numbers from the match and store them in a list of dictionaries
+    plans = []
+    for match in matches:
+        plan = {
+            'add': int(match[0]),
+            'change': int(match[1]),
+            'destroy': int(match[2]),
+            'resources': {}
+        }
+        plans.append(plan)
 
 # Loop through each line and add the resources to the correct plan
-current_plan = 0
-for line in output_str.split("\n"):
-    match = re.search(r'^\s*\#\s+(\w+)\.(\w+)\s+will be (\w+)', line)
-    if match:
-        resource_type = match.group(1)
-        resource_name = match.group(2)
-        resource_action = match.group(3)
-        resource_key = f"{resource_type}.{resource_name}"
-        plans[current_plan]['resources'][resource_key] = resource_action
-    else:
-        # Check if the line contains a new plan
-        match = re.search(r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', line)
+    current_plan = 0
+    for line in output_str.split("\n"):
+        match = re.search(r'^\s*\#\s+(\w+)\.(\w+)\s+will be (\w+)', line)
         if match:
-            current_plan += 1
+            resource_type = match.group(1)
+            resource_name = match.group(2)
+            resource_action = match.group(3)
+            resource_key = f"{resource_type}.{resource_name}"
+            plans[current_plan]['resources'][resource_key] = resource_action
+        else:
+            #  Check if the line contains a new plan
+            match = re.search(r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', line)
+            if match:
+                current_plan += 1
 
-print(plans)
+    print(plans)
 
+run_all_plan()
 
 
 
